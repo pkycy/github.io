506e25399b4dbe8fc79b544a59aea1308ce440f1
PKYCY <13060626770@163.com>
2024-11-29 17:33:04 +0800

    Site updated: 2024-11-29 17:33:04

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const preCache = [
   "/images/taichi.png",
-  "/images/jianlai.webp",
+  "/images/p0.webp",
   "/images/taichi.png",
   "/css/loader.css",
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732862434642";
+const VERSION = "1732872776899";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
