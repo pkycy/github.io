9f22de203d47d064f69b68243e65430d72710f6b
PKYCY <13060626770@163.com>
2025-08-31 00:41:33 +0800

    Site updated: 2025-08-31 00:41:32

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1756571442334";
+const VERSION = "1756572092160";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
