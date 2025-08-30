0064e0440f1d18e21426d3e4cac14560069ec4c1
PKYCY <13060626770@163.com>
2025-08-31 00:44:39 +0800

    新增文章继续博客之旅

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733065157479";
+const VERSION = "1756572092160";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
